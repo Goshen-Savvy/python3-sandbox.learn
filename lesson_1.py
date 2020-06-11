4ca28033359e62af311ece7ed028a98e5fc44788
Goshen <aaodeh@mun.ca>
2020-06-11 10:02:35 -0230

    initial commit

--- lesson_1.py
+++ lesson_1.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-#Checks if a number exist in a string
+#Checks if a number exist in a list
 
 class Lesson:
     def check(data, num):
@@ -9,19 +8,3 @@ class Lesson:
                 print("Lucky number")
             else:
                 print("try again")
-
-
-=======
-#Checks if a number exist in a string
-
-class Lesson:
-    def check(data, num):
-        print (data = [3,8,9,3,5,8,2], 5)
-        for f in num:
-            if (f == 5):
-                print("Lucky number")
-            else:
-                print("try again")
-
-
->>>>>>> 24fdb9fcbf9ffb4af34d6403a3a15db4a7aaa26e
