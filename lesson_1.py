24fdb9fcbf9ffb4af34d6403a3a15db4a7aaa26e
Goshen-Savvy <65304434+Goshen-Savvy@users.noreply.github.com>
2020-06-11 09:44:18 -0230

    Add files via upload

---
+++ lesson_1.py
@@ -0,0 +1,12 @@
+#Checks if a number exist in a string
+
+class Lesson:
+    def check(data, num):
+        print (data = [3,8,9,3,5,8,2], 5)
+        for f in num:
+            if (f == 5):
+                print("Lucky number")
+            else:
+                print("try again")
+
+
